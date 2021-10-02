347d58a9af91268628270ec8913a8487953726cf
Riju Dasgupta <63913601+RijuDasgupta9116@users.noreply.github.com>
2021-10-02 21:08:19 +0530

    adding index.html, script.js and console_log.PNG

---
+++ JS SESSON 1/script.js
@@ -0,0 +1,12 @@
+// this console.clear function will clear the console screen
+console.clear()
+
+
+var num1 = 3;
+var num2 = 4;
+var num3 = 13;
+
+var sum = num1 + num2 + num3;
+
+// console.log() will print the sum or any kind of printing
+console.log(sum);
