dd23bba875e248fad7cf42c65510a2ff172cef8e
Riju Dasgupta <63913601+RijuDasgupta9116@users.noreply.github.com>
2021-10-08 11:41:15 +0530

    adding index.html and script.js

---
+++ JS SESSION 4/script.js
@@ -0,0 +1,38 @@
+var age = prompt("Enter your age : ");
+if (age >= 18 && age < 60) {
+    console.log('You can vote');
+} else if (age >= 60 && age <= 100) {
+    console.log('You are a senior citizen');
+} else {
+    console.log('You can\'t vote');
+}
+
+
+/* Comment the upper code and  Uncomment the below code and run it */
+
+//var day = prompt('Enter day (Mon,Tues,...) : ');
+//switch (day) {
+//    case 'Mon':
+//        console.log('Monday');
+//        break;
+//    case 'Tues':
+//        console.log('Tuesday');
+//        break;
+//    case 'Wedness':
+//        console.log('Wednessday');
+//        break;
+//    case 'Thus':
+//        console.log('Thusday');
+//        break;
+//    case 'Fri':
+//        console.log('Friday');
+//        break;
+//    case 'Satur':
+//        console.log('Saturday');
+//        break;
+//    case 'Sun':
+//        console.log('Sunday');
+//        break;
+//    default:
+//        console.log('Wrong Prompt');
+//}
